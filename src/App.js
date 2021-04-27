e4f3ec5a1b5088c2ca97af408e4102093929c993
Ashish <ashish.deora@greatplacetowork.com>
2021-04-28 00:14:21 +0530

    Hooks Tutorial - Completed Accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a front-end library"
+    },
+    {
+        title: "How to use react?",
+        content: "You can use react by creating components"
+    },
+    {
+        title: "Is react better than Angular?",
+        content: "You cannot compare apple with oranges"
+    }
+];
+
+const App = () => {
+    return(
+    <div>
+        <Search />
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+
+const Search = () => {
+
+    const [term, setTerm] = useState('');
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search term</label>
+                    <input
+                    value={term}
+                    onChange={e => {setTerm(e.target.value)}}
+                    className="input" 
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Search;
